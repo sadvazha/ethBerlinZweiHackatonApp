c0170a213b3d6415ae47019960f31e2367255f1d
zhan <zhan@suite.st>
2019-08-24 03:11:02 +0200

    Integration with meta-stamp

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,27 @@ const bodyParser = require('body-parser');
 const config = require('./config');
 const constants = require('./constants');
 const express = require('express');
+const metaStamp = require('meta-stamp');
 const utils = require('./utils');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+metaStamp.init({
+    version: '1.0.0',
+    domain: 'https://trade.dib.one',
+    logo: 'https://dib.one/static/log.png',
+    hook: 'https://api.dib.one/v1/signatures/submit',
+});
+
 app.get('/', (req, res) => {
-    return res.send({
-        id: 'some id',
-        version: '1.0.0',
-        domain: 'trade.dib.one',
-        logo: 'https://dib.one/static/log.png',
-        description: 'Test description',
-        hook: '',
+    return res.send(metaStamp.generate({
+        id: '123',
+        description: 'Test',
         data: {},
-        metaData: {},
-    });
+        metadata: {},
+    }));
 });
 
 app.post('/', (req, res) => {
